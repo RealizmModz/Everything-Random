1db5f823f0a12f501517cdcd59d6bd0484f63cfd
RealizmModz <74314445+RealizmModz@users.noreply.github.com>
2022-03-04 16:21:15 +0000

    Added Random Number and Random Array Object

---
+++ src/classes/functions.js
@@ -0,0 +1,36 @@
+function randomNumber(min,max,round) {
+	try {
+		if(typeof min != "number" || typeof max != "number") return console.log('Please enter the minimum and maximum number to get a random in between!');
+		if(!round) { round = false;}
+		if(typeof round != "boolean") return console.log('Enter a boolean!');
+		
+		if(round == true) {
+			return Math.round(min + Math.random() * (max - min));
+		}
+		else {
+			return min + Math.random() * (max - min);
+		};
+	}
+	catch(error) {
+		console.log(error);
+	};
+};
+
+function randomArrayObject(array) {
+	try{
+		if(!array) return console.log("You need to enter the array!")
+		if(typeof Array.isArray(array) == false) return console.log('This must be an array!');
+
+		const index = Math.round(0 + Math.random() * (array.length));
+
+		return array[index];
+	}
+	catch(error) {
+		console.log(error);
+	}
+}
+
+module.exports = {
+	randomNumber,
+	randomArrayObject
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const everythingrandom = require('./classes/functions.js');
+
+const array = [
+	"Hi!",
+	"Hello!",
+	"How's it going?",
+	"Lovely weather we're having!"
+]
+console.log(everythingrandom.randomArrayObject(array))
